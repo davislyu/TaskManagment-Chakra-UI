77c3fadd9f4cea9bb3fba69710970009c94856fe
davislyu <davislyu@gmail.com>
2023-11-01 21:25:21 +0200

    Added action

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,81 @@
+import { Box, Button, Checkbox, Flex, FormControl, FormHelperText, FormLabel, Input, Textarea } from "@chakra-ui/react";
+import { Form, redirect } from "react-router-dom";
+import { useState } from "react";
+
 export default function Create() {
+  const [isEmptyName, setIsEmptyName] = useState(true);
+  const [isEmptyNameDescription,SetIsEmptyNameDescription] = useState(true);
+
+
+const handleNameChange = (e) => {
+  setIsEmptyName(e.target.value.trim() ==="");
+}
+
+
+const handleDescriptionChange = (e) => {
+  SetIsEmptyNameDescription(e.target.value.trim() ==="");
+}
+
+
+
   return (
-    <div>Create</div>
-  )
+    <Box maxW="480px">
+      <Form method="post" action="/create">
+        <FormControl isRequired={isEmptyName} mb="40px">
+          <Flex justifyContent="space-between">
+                      <FormLabel>Task Name:</FormLabel> 
+                      {isEmptyName && <FormHelperText>Cannot be empty</FormHelperText>}
+
+
+          </Flex>
+          <Input 
+            type="text" 
+            name="title" 
+            onChange={handleNameChange} 
+            placeholder="Enter task name"
+          />
+          <FormHelperText>Enter a descriptive task name</FormHelperText>
+        </FormControl>
+        <FormControl isRequired={isEmptyNameDescription}>
+        <Flex justifyContent="space-between">
+
+          <FormLabel >
+            Task description
+          </FormLabel>
+          {isEmptyNameDescription && <FormHelperText>Cannot be empty</FormHelperText>}
+          </Flex>
+          
+          <Textarea             name="description" 
+           onChange={handleDescriptionChange} placeholder="Enter a detailed description" />
+        </FormControl>
+        <FormControl        
+          mt="10px" 
+          display="flex"
+          alignItems="center">
+          <Checkbox
+          colorScheme="purple"
+          name="isPriority"
+          size="lg"/>
+          <FormLabel mb="0px" ml="10px">Make this a priority task.</FormLabel>
+        </FormControl>
+        <Button
+        mt="40px"
+        type="submit"
+        colorScheme="purple"
+        >Submit</Button>
+      </Form>
+    </Box>
+  );
+}
+
+
+export const createAction = async ({request}) =>{
+  const data = await request.formData()
+  const task = {
+    title : data.get("title"),
+    description : data.get("description"),
+    isPriority:data.get("isPriority") ===""
+  }
+  console.log(task)
+  return redirect("/")
 }
