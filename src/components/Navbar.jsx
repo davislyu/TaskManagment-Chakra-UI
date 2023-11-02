52e31d245c7bd17bf83b793bd2957e93f16a1f43
davislyu <davislyu@gmail.com>
2023-11-02 10:44:26 +0200

    Added toast notifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,40 @@
-import { Flex,Box, Heading,Text,Button, Spacer, HStack } from '@chakra-ui/react'
-import React from 'react'
-
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Spacer,
+  HStack,
+  useToast,
+} from "@chakra-ui/react";
+import React from "react";
 
 export default function Navbar() {
+  const toast = useToast();
+
   return (
     <Flex as="nav" mb="40px" p="10px" alignItems="center" gap="10px">
-        <Heading as="h1">
-            Dojo Tasks
-        </Heading>
-        <Spacer/>
-        <HStack spacing="20px">
-        <Box bg="gray.200" p="10px">M</Box>
+      <Heading as="h1">Dojo Tasks</Heading>
+      <Spacer />
+      <HStack spacing="20px">
+        <Box bg="gray.200" p="10px">
+          M
+        </Box>
         <Text>Davis@gmail.com</Text>
-        <Button colorScheme='purple'>LogOut</Button>
-        </HStack>
+        <Button
+          colorScheme="purple"
+          onClick={() => {
+            toast({
+              title: `Logged out successfully`,
+              position: "top",
+              isClosable: true,
+            });
+          }}
+        >
+          LogOut
+        </Button>
+      </HStack>
     </Flex>
-  )
+  );
 }
